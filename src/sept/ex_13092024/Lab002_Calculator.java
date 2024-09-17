b803bce446f1fec694901bdc23ca86c2f6d3fada
Navya <sainavya583@gmail.com>
2024-09-17 16:15:34 +0530

    13th tasks

---
+++ src/sept/ex_13092024/Lab002_Calculator.java
@@ -0,0 +1,20 @@
+package sept.ex_13092024;
+
+public class Lab002_Calculator {
+
+    public static void main(String[] args) {
+        int a=10, b=20;
+        int add= 10+20;
+        int sub=10-20;
+        int mul=10*20;
+        int div=10/20;
+        int mod=10%20;
+        System.out.printf("%d and %d sum is = %d\n", a, b, add);
+        System.out.printf("%d and %d sub is = %d\n", a, b, sub);
+        System.out.printf("%d and %d mul is = %d\n", a, b, mul);
+        System.out.printf("%d and %d div is = %d\n", a, b, div);
+        System.out.printf("%d and %d mod is = %d\n", a, b, mod);
+
+
+    }
+}
